cf45122462b492579221f02073866644d28132ae
Atsuko Shimizu <atskae@gmail.com>
2021-10-23 07:33:50 -0400

    Add command line args to read ROM

--- src/cpu_core/cpu.rs
+++ src/cpu_core/cpu.rs
@@ -1,6 +1,10 @@
+use log::{debug, info, warn};
 use std::fmt;
 use std::format;
+use std::fs;
+use std::path::PathBuf;
 
+use crate::cli::Subcommand;
 use crate::cpu_core::register::{Register, RegisterOperation};
 
 #[derive(Default)] // needed so Register initalizes to zero automatically
@@ -14,12 +18,16 @@ pub struct Cpu {
     /// Stack pointer and program counter
     sp_reg: Register,
     pc_reg: Register,
+
+    // Loaded ROM
+    rom: Vec<u8>,
 }
 
 impl fmt::Display for Cpu {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let registers = format!(
             "
+            ROM: {} bytes
             Registers
             AF: {} {}
             BC: {} {}
@@ -28,6 +36,7 @@ impl fmt::Display for Cpu {
             stack_pointer: {}
             program_counter: {}
             ",
+            self.rom.len(),
             self.af_reg.read_upper(),
             self.af_reg.read_lower(),
             self.bc_reg.read_upper(),
@@ -44,7 +53,22 @@ impl fmt::Display for Cpu {
 }
 
 impl Cpu {
-    pub fn new() -> Cpu {
-        Default::default()
+    pub fn new(rom_path: PathBuf) -> Cpu {
+        let mut cpu: Cpu = Default::default();
+        if rom_path.exists() {
+            cpu.rom = fs::read(rom_path).unwrap();
+            debug!(
+                "Loaded ROM (byte preview): {:#02x} {:#02x} {:#02x}",
+                cpu.rom[0], cpu.rom[1], cpu.rom[2]
+            );
+        } else {
+            warn!("ROM file does not exist! Nothing was loaded.");
+        }
+
+        cpu
+    }
+
+    pub fn start(&self, subcommand: Subcommand) {
+        info!("Subcommand: {:?}", subcommand);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+mod cli;
 mod cpu_core;
 
 use crate::cpu_core::cpu::Cpu;
+use cli::CommandLineArgs;
 use log::{debug, info};
 
 fn main() {
     env_logger::init();
     info!("Starting rusty-gameboy 🦀🎮");
-    let cpu = Cpu::new();
+    let args = CommandLineArgs::new();
+    debug!("Command line args: {:?}", args);
+
+    let cpu = Cpu::new(args.rom_path);
     debug!("Created a CPU object {}", cpu);
+    cpu.start(args.subcommand);
 }
