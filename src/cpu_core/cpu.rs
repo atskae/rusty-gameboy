6f5cc86016cece07a1ca08547763497e4b47e518
Atsuko Shimizu <atskae@gmail.com>
2021-10-25 13:49:05 -0400

    Fix match z and y order

--- src/cpu_core/cpu.rs
+++ src/cpu_core/cpu.rs
@@ -21,6 +21,7 @@ enum RegIndex {
     SP,
     /// Program counter
     PC,
+    NumRegs,
 }
 
 impl Index<RegIndex> for Vec<Register> {
@@ -45,6 +46,11 @@ pub struct Cpu {
 
 impl fmt::Display for Cpu {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        debug!(
+            "{} registers, AF={}",
+            self.regs.len(),
+            RegIndex::AF as usize
+        );
         let registers = format!(
             "
             ROM: {} bytes
@@ -74,7 +80,18 @@ impl fmt::Display for Cpu {
 
 impl Cpu {
     pub fn new(rom_path: PathBuf) -> Cpu {
-        let mut cpu: Cpu = Default::default();
+        let mut cpu: Cpu = Cpu {
+            regs: Vec::with_capacity(RegIndex::NumRegs as usize), // only sets upper bound
+            ..Default::default()
+        };
+
+        // Initialize registers
+        for _ in 0..RegIndex::NumRegs as usize {
+            cpu.regs.push(Default::default());
+        }
+        debug!("Initialized {} registers", cpu.regs.len());
+
+        // Load ROM
         if rom_path.exists() {
             cpu.rom = fs::read(rom_path).unwrap();
             debug!(
@@ -126,6 +143,7 @@ impl Cpu {
     /// If cond is true, check condition of the flag (specified by y)
     /// to decide whether to jump or not
     fn jr_d8(&mut self, cond: bool, y: u8) -> u16 {
+        debug!("Jump, cond={}, y={}", cond, y);
         let pc_increment = 0;
         if cond && !self.cond_code(y - 4) {
             return pc_increment;
@@ -133,12 +151,14 @@ impl Cpu {
 
         let pc = self.regs[RegIndex::PC].read() as usize; // points to the opcode
         let displacement: i8 = self.rom[pc + 1] as i8;
+        debug!("displacement: {}", displacement);
 
         let mut new_pc = pc as u16;
+        debug!("pc={}, new_pc={}", pc, new_pc);
         if displacement < 0 {
-            new_pc -= displacement as u16;
+            new_pc -= displacement.abs() as u16;
         } else {
-            new_pc += displacement as u16;
+            new_pc += displacement.abs() as u16;
         }
         self.regs[RegIndex::PC].write(new_pc);
 
@@ -152,26 +172,34 @@ impl Cpu {
         // Decode the opcode byte by reading the subfields according to:
         // https://gb-archive.github.io/salvage/decoding_gbz80_opcodes/Decoding%20Gamboy%20Z80%20Opcodes.html
         let opcode_byte: u8 = self.rom[self.regs[RegIndex::PC].read() as usize];
+        debug!("program_counter: {}", self.regs[RegIndex::PC].read());
+        debug!("Opcode {:b}", opcode_byte);
 
         let x: u8 = (opcode_byte & 0b1100_0000) >> 6;
         let y: u8 = (opcode_byte & 0b0011_1000) >> 3;
-        //let z: u8 = opcode_byte & 0b0000_0011;
+        let z: u8 = opcode_byte & 0b0000_0011;
         //let p: u8 = (y & 0b110) >> 1;
         //let q: u8 = y & 0b001;
 
         // Unprefixed opcodes
         let pc_increment: u16 = match x {
             0 => {
-                match y {
-                    0 => 1,                    // NOP
-                    1 => self.load_imm16_sp(), // Load immediate into SP
-                    2 => 2,                    // STOP
-                    3 => self.jr_d8(false, y),
-                    _ => self.jr_d8(true, y), // Conditional jump
+                match z {
+                    0 => match y {
+                        0 => 1,                    // NOP
+                        1 => self.load_imm16_sp(), // Load immediate into SP
+                        2 => 2,                    // STOP
+                        3 => self.jr_d8(false, y), // Jump
+                        _ => self.jr_d8(true, y),  // Conditional jump
+                    },
+                    _ => {
+                        warn!("Not implemented this case of z!");
+                        0
+                    }
                 }
             }
             _ => {
-                debug!("Not implemented yet!");
+                debug!("Not implemented this case of x!");
                 0
             }
         };
@@ -182,6 +210,8 @@ impl Cpu {
 
     pub fn start(&mut self, subcommand: Subcommand) {
         info!("Subcommand: {:?}", subcommand);
+
+        info!("Running execute()");
         self.execute();
     }
 }
